49b44b84755940836a948c684bf721bd5207ff78
Alexandra Olson <alolson2017@gmail.com>
2019-03-03 22:52:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-
 function theBeatlesPlay(musicians, instruments) {
   const empty = []
 
@@ -37,59 +35,4 @@ function iLoveTheBeatles(number) {
   } while (number < 15)
 
   return empty
-=======
-const musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
-
-const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
-
-function theBeatlesPlay(musicians, instruments) {
-	var beatles = [];
-	for (var i = 0; i<musicians.length; i++) {
-	beatles.push(musicians[i] + " plays " + instruments[i]);
-	}
-	return(beatles);
-}
-
-/////
-const facts = [
-  "He was the last Beatle to learn to drive",
-  "He was never a vegetarian",
-  "He was a choir boy and boy scout",
-  "He hated the sound of his own voice"
-];
-
-function johnLennonFacts(facts){
-  
-var i = 0;
-	var lennon = facts.length
-	var morelennon = [];
-
-	while (lennon > 0) {
-		morelennon.push(facts[i] + "!!!");
-		i++;
-		lennon--;
-	}
-	return(morelennon);
-}
-////////
-
-
-function iLoveTheBeatles(number){
-  var empty = [];
-  number++;
-  
-	var i = 0;
-	do {
-		empty.push("I love the Beatles!");
-		i++;
-
-	} while (i < number);
-	
-if (number === 18) {
-		return("I love the Beatles!");
-	}
-	
-	return(empty);
-  
->>>>>>> e6b4a486a8c4d81f739581c6daa1c4295da6e1d7
 }
